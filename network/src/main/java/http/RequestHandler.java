b46a7967d85a621783074cda0d4786a2db54db80
Midichi <8282qwe@gmail.com>
2024-11-28 17:47:53 +0900

    11/28 Serving 404.html 400.html

--- network/src/main/java/http/RequestHandler.java
+++ network/src/main/java/http/RequestHandler.java
@@ -54,16 +54,17 @@ public class RequestHandler extends Thread {
             } else {
                 // method : POST, DELETE, PUT, HEAD, CONNECT, ...
                 // (현재 SimpleHttpServer에서는 무시) GET method 이외의 것이 들어온 경우, Bad Request(400) 응답
+                // nio
+                File file = new File(BASE_URL+"/error/400.html");
+                byte[] body = Files.readAllBytes(file.toPath());
+                String contentType = Files.probeContentType(file.toPath());
+
                 outputStream.write((tokens[2] + " 400 Bad Request\n").getBytes(StandardCharsets.UTF_8));
-                outputStream.write("Content-Type:text/html; charset=utf-8\n".getBytes(StandardCharsets.UTF_8));
+                outputStream.write(("Content-Type:"+contentType+"; charset=utf-8\n").getBytes(StandardCharsets.UTF_8));
                 outputStream.write("\n".getBytes());
+                outputStream.write(body);
+
             }
-            // 예제 응답입니다.
-            // 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-//            outputStream.write("HTTP/1.1 200 OK\n".getBytes(StandardCharsets.UTF_8));
-//            outputStream.write("Content-Type:text/html; charset=utf-8\n".getBytes(StandardCharsets.UTF_8));
-//            outputStream.write("\n".getBytes());
-//            outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes(StandardCharsets.UTF_8));
         } catch (Exception ex) {
             consoleLog("error:" + ex);
         } finally {
@@ -85,9 +86,14 @@ public class RequestHandler extends Thread {
 
         if (!file.exists()) {
             // 404 response
+            file = new File(BASE_URL+"/error/404.html");
+            byte[] body = Files.readAllBytes(file.toPath());
+
+            String contentType = Files.probeContentType(file.toPath());
             os.write((protocol + " 404 Not Found\n").getBytes(StandardCharsets.UTF_8));
-            os.write("Content-Type:text/html; charset=utf-8\n".getBytes(StandardCharsets.UTF_8));
+            os.write(("Content-Type:"+contentType+"; charset=utf-8\n").getBytes(StandardCharsets.UTF_8));
             os.write("\n".getBytes());
+            os.write(body);
             return;
         }
 
